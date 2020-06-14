4c224aca48c61f44348a001a653852786628ab0d
Santosh Bhattarai <hsotnas76@gmail.com>
2020-06-14 08:54:36 +0545

    initial commit

---
+++ lib/model/json_parsing.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class JsonParsingSimple extends StatefulWidget {
+  @override
+  _JsonParsingSimpleState createState() => _JsonParsingSimpleState();
+}
+
+class _JsonParsingSimpleState extends State<JsonParsingSimple> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Networking'),
+      ),
+    );
+  }
+}

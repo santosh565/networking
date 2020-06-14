b7bb3f12f82f475edeff0241f88e757a9dffa034
Santosh Bhattarai <hsotnas76@gmail.com>
2020-06-14 09:19:38 +0545

    setup network class

--- lib/model/json_parsing.dart
+++ lib/model/json_parsing.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart';
 
 class JsonParsingSimple extends StatefulWidget {
   @override
@@ -10,8 +11,26 @@ class _JsonParsingSimpleState extends State<JsonParsingSimple> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Networking'),
+        title: Text('Json Parsing'),
       ),
     );
+  }
+}
+
+class Network {
+  final String url;
+
+  Network(this.url);
+
+  Future getData() async {
+    print('$url');
+
+    Response response = await get(Uri.encodeFull(url));
+
+    if (response.statusCode == 200) {
+      return response.body;
+    } else {
+      print(response.statusCode);
+    }
   }
 }

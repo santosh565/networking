d1977e919478643641280e35719559b8864f1b86
Santosh Bhattarai <hsotnas76@gmail.com>
2022-02-10 22:32:15 +0545

    update

--- lib/response/user_model.dart
+++ lib/response/user_model.dart
@@ -6,23 +6,18 @@ part 'user_model.freezed.dart';
 part 'user_model.g.dart';
 
 @freezed
-class User with _$User{
+class User with _$User {
   const User._();
 
   // ignore: invalid_annotation_target
   @JsonSerializable(explicitToJson: true)
-
-  const factory User(
-    {
-      required String name,
-      int? age,
-      required List<Job> jobs,
-    }
-  ) = _User;
-
+  const factory User({
+    required String name,
+    int? age,
+    required List<Job> jobs,
+  }) = _User;
 
   factory User.fromJson(Map<String, dynamic> json) => _$UserFromJson(json);
 
   String forFun() => '$name "hello" $age';
-
 }

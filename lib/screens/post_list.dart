dcdcda60289a4e94ca0990dc4558f47ff2c9b908
Santosh Bhattarai <hsotnas76@gmail.com>
2022-02-20 14:20:44 +0545

    update

--- lib/screens/post_list.dart
+++ lib/screens/post_list.dart
@@ -21,30 +21,32 @@ class _PostsListState extends State<PostsList> {
 
   @override
   Widget build(BuildContext context) {
-    return BlocBuilder<PostBloc, PostState>(
-      builder: (context, state) {
-        switch (state.status) {
-          case PostStatus.failure:
-            return Center(child: Text(state.error!));
-          case PostStatus.success:
-            if (state.posts.isEmpty) {
-              return const Center(child: Text('no posts'));
-            }
-            return ListView.builder(
-              itemBuilder: (BuildContext context, int index) {
-                return index >= state.posts.length
-                    ? const BottomLoader()
-                    : PostListItem(post: state.posts[index]);
-              },
-              itemCount: state.hasReachedMax
-                  ? state.posts.length
-                  : state.posts.length + 1,
-              controller: _scrollController,
-            );
-          default:
-            return const Center(child: CircularProgressIndicator());
-        }
-      },
+    return Scaffold(
+      body: BlocBuilder<PostBloc, PostState>(
+        builder: (context, state) {
+          switch (state.status) {
+            case PostStatus.failure:
+              return Center(child: Text(state.error!));
+            case PostStatus.success:
+              if (state.posts.isEmpty) {
+                return const Center(child: Text('no posts'));
+              }
+              return ListView.builder(
+                itemBuilder: (BuildContext context, int index) {
+                  return index >= state.posts.length
+                      ? const BottomLoader()
+                      : PostListItem(post: state.posts[index]);
+                },
+                itemCount: state.hasReachedMax
+                    ? state.posts.length
+                    : state.posts.length + 1,
+                controller: _scrollController,
+              );
+            default:
+              return const Center(child: CircularProgressIndicator());
+          }
+        },
+      ),
     );
   }
 
